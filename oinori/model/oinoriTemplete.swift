5557025052bf9ab225b12a160cbbbcf1395e8c94
yodaaa <01ewell0@gmail.com>
2019-06-17 01:21:07 +0900

    add Realm

---
+++ oinori/model/oinoriTemplete.swift
@@ -0,0 +1,20 @@
+//
+//  oinoriTemplete.swift
+//  oinori
+//
+//  Created by yodaaa on 2019/06/17.
+//  Copyright © 2019 yodaaa. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class OinoriTemplete: Object {
+    dynamic var id: Int = 0
+    dynamic var title: String = ""
+    dynamic var message: String = ""
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
