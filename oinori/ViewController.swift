75f40644de501c21ca8852e054a633a036569377
yodaaa <01ewell0@gmail.com>
2019-06-16 21:29:50 +0900

    Initial Commit

---
+++ oinori/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oinori
+//
+//  Created by yodaaa on 2019/06/16.
+//  Copyright © 2019 yodaaa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
